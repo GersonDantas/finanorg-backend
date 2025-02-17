d5102d41cdd300a028dc9ef745c24c5172070e03
gersondantas <53773325+GersonDantas@users.noreply.github.com>
2025-02-17 19:06:10 -0300

    chore: add npm

--- src/config/config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  "development": {
-    "username": "postgres",
-    "password": "T&3d2i",
-    "database": "financas_db",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
-  }
-} 
