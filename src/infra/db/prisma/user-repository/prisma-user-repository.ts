4a63c8d6060910e4e3bcdf63470705aff29ce864
gersondantas <53773325+GersonDantas@users.noreply.github.com>
2025-02-18 20:44:40 -0300

    feat: implement PrismaUserRepository

---
+++ src/infra/db/prisma/user-repository/prisma-user-repository.ts
@@ -0,0 +1,16 @@
+import type { CreateUserRepository } from '@/data/protocols/db/user/create-user-repository'
+import type { User } from '@/domain/models/user';
+import { prisma } from '@/infra/db/prisma/helpers/prisma-helper'
+
+export class PrismaUserRepository implements CreateUserRepository {
+  async create (data: CreateUserRepository.Params): Promise<CreateUserRepository.Result> {
+    const user: User = await prisma.user.create({
+      data: {
+        name: data.name,
+        email: data.email,
+        password: data.password
+      }
+    })
+    return user
+  }
+} 
