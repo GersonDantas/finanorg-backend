c616a4c48f32f4576b792ee27ea6052e87c7f58d
gersondantas <53773325+GersonDantas@users.noreply.github.com>
2025-02-18 16:32:35 -0300

    chore:  eslint config rules

--- eslint.config.ts
+++ eslint.config.ts
@@ -5,5 +5,13 @@ export default [
     ...love,
     files: ['**/*.ts'],
     ignores: ['node_modules/**/*'],
+    rules: {
+      ...love.rules,
+      '@typescript-eslint/strict-boolean-expressions': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-magic-numbers': 'off',
+      '@typescript-eslint/class-methods-use-this': 'off',
+    },
   },
 ]
